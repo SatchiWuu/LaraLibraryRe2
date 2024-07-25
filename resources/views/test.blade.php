83180900c442d8035d011faf6448ee88086d2512
SatchiWuu <159090024+SatchiWuu@users.noreply.github.com>
2024-07-25 20:21:00 +0800

    Create test.blade.php

---
+++ resources/views/test.blade.php
@@ -0,0 +1,14 @@
+<div style="width:100%; display:flex; justify-content:space-between; align-items:center">
+    <div class="text-container">
+        <h1>Welcome to</h1>
+        <h2>GlitzVouge</h2>
+        <p>We cater to individuals regardless of gender to suffice <br>
+            your needs and wants in terms of cosmetic products</p>
+        <a href="/products" class="btn btn-primary mt-4">Shop Now!</a>
+    </div>
+
+    <div class="picture" style="height:100%;width:70%;">
+        <img style=" width: 100%;height: 100%;object-fit: contain;overflow: hidden;border-radius: 20px;" src="{{ asset('images/puririkaa.jpg') }}" alt="homepage">
+    </div>
+</div>
+
